2906b1d6d8350bff42f63d1cbdb2e2ebaaed9366
kaito913ps <kaito913jp@gmail.com>
2023-11-07 10:18:01 +0900

    stage1をほぼ作った

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,22 @@
-using System.Collections;
+using DG.Tweening;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static Walkable;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool walking = false;
     [Space]
     [Header("キューブの情報")]
-    [SerializeField, Tooltip("現在位置するキューブ")]
+    [SerializeField, Tooltip("現在の位置にあるキューブ")]
     private Transform currentCube;
     [SerializeField, Tooltip("マウスクリックしたキューブ")]
     private Transform clickedCube;
 
     [Space]
-    [Header("パスファインディング")]
-    //プレイヤーが実際に移動するパス
+    [Header("経路")]
+    //プレイヤーが実際に移動する経路
     public List<Transform> finalPath = new List<Transform>();
 
 
@@ -22,57 +24,73 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //プレイヤーが踏んでいるキューブの設定
         RayCastDown();
     }
 
     // Update is called once per frame
+
     void Update()
     {
+        //プレイヤーが踏んでいるキューブの設定
         RayCastDown();
 
-        //現在踏んでいるキューブが動く場合
-        if(currentCube.GetComponent<Walkable>().movingGround)
+        ////現在踏んでいるキューブが動く場合
+        if (currentCube.GetComponent<Walkable>().movingGround)
         {
+
             //プレイヤーをその子に入れる
             transform.parent = currentCube.parent;
         }
         else
         {
-            transform .parent = null;
+            transform.parent = null;
         }
 
         //マウスクリックチェック
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit mouseHit;
 
-            //レイ打ち上げる
-            if(Physics.Raycast(mouseRay, out mouseHit))
+            //レイイを発射!!
+            if (Physics.Raycast(mouseRay, out mouseHit))
             {
                 //クリックした場所がPathの場合
-                if(mouseHit.transform.GetComponent<Walkable>() != null)
+                if (mouseHit.transform.GetComponent<Walkable>() != null)
                 {
                     //クリックしたキューブの位置を設定
+                    clickedCube = mouseHit.transform;
+                    FindPath();
                 }
             }
         }
     }
 
     /// <summary>
-    /// 道を探す
+    ///経路探索
     /// </summary>
     private void FindPath()
     {
-        //次を移動するキューブ
+        //次に移動するキューブ
         List<Transform> nextCubes = new List<Transform>();
         //前のキューブ
-        List<Transform> pastCubes = new List<Transform> ();
+        List<Transform> pastCubes = new List<Transform>();
 
-        
+        //現在のキューブに接続されたキューブの数だけループ
+        foreach (WalkPath path in currentCube.GetComponent<Walkable>().possiblePaths)
+        {
+            if (path.active)
+            {
+                nextCubes.Add(path.target);
+                path.target.GetComponent<Walkable>().previousBlock = currentCube;
+            }
+        }
 
         pastCubes.Add(currentCube);
 
+        ExploreCube(nextCubes, pastCubes);
+        BuildPath();
     }
 
     private void ExploreCube(List<Transform> nextCubes, List<Transform> visitedCubes)
@@ -81,49 +99,115 @@ public class PlayerController : MonoBehaviour
         nextCubes.Remove(current);
 
         //クリックしたキューブと現在のキューブが同じ場合
-        //目標座標に到着したもの
-        if(current == clickedCube)
+        //目標座標に到達した場合
+        if (current == clickedCube)
         {
             return;
         }
-        //訪問したキューブリストに現在のキューブを追加する
+
+        //現在のキューブの移動可能なキューブの数だけ繰り返す
+        foreach (WalkPath path in current.GetComponent<Walkable>().possiblePaths)
+        {
+            //既に通過した道でなく、かつ道が接続されている場合
+            if (!visitedCubes.Contains(path.target) && path.active)
+            {
+                //次の検索キューブに移動経路に追加」
+                nextCubes.Add(path.target);
+                //次に探索するキューブを移動経路に追加
+                path.target.GetComponent<Walkable>().previousBlock = current;
+            }
+        }
+        //訪れたキューブのリストに現在のキューブを追加
         visitedCubes.Add(current);
+
+        //リストが1つでもある場合
+        if (nextCubes.Any())
+        {
+            ExploreCube(nextCubes, visitedCubes);
+        }
     }
 
     /// <summary>
-    /// パスの作成
+    /// 経路の生成
     /// </summary>
     private void BuildPath()
     {
+        Debug.Log("Building Path");
         Transform cube = clickedCube;
 
-        //クリックしたキューブが現在のキューブと等しくないまで
-        while(cube != currentCube)
+        //リックしたキューブが現在のキューブと同じでない限り」
+        while (cube != currentCube)
         {
-            //実際に移動するパスに挿入
+            Debug.Log("Inside loop: " + cube.name);
+            //実際に移動する経路に挿入
             finalPath.Add(cube);
-            //クリックしたキューブの前のキューブがNullの時
-            if(cube.GetComponent<Walkable>().previousBlock != null)
+            //クリックしたキューブの前のキューブがNullの場合」
+            if (cube.GetComponent<Walkable>().previousBlock != null)
             {
+                Debug.Log("Inside loop: " + cube.name);
                 cube = cube.GetComponent<Walkable>().previousBlock;
             }
             else
             {
+                Debug.Log("Inside loop: " + cube.name);
                 return;
             }
         }
+
+        FollowPath();
     }
 
     private void FollowPath()
     {
 
+        Sequence s = DOTween.Sequence();
+
+        walking = true;
+
+        for (int i = finalPath.Count - 1; i > 0; i--)
+        {
+            float time = finalPath[i].GetComponent<Walkable>().isStair ? 1.5f : 1;
+
+            s.Append(transform.DOMove(finalPath[i].GetComponent<Walkable>().GetWalkPoint(), .2f * time).SetEase(Ease.Linear));
+        }
+        s.Append(transform.DOMove(clickedCube.GetComponent<Walkable>().GetWalkPoint(), .2f).SetEase(Ease.Linear));
+        s.AppendCallback(() => Clear());
+    }
+
+    void Clear()
+    {
+        foreach (Transform t in finalPath)
+        {
+            t.GetComponent<Walkable>().previousBlock = null;
+        }
+        finalPath.Clear();
+        walking = false;
     }
 
     /// <summary>
-    /// 現在プレイヤーが踏んでいるキューブを探す関数
+    ///プレイヤーが現在踏んでいるキューブを見つける関数
     /// </summary>
     public void RayCastDown()
     {
+        //プレイヤーの中心座標を生成
+        //Vector3 rayPos = transform.position;
+        //rayPos.y += transform.localScale.y * 0.5f;
 
+        ////レイを作成し、方向は下向き
+        //Ray playerRay = new Ray(rayPos, -transform.up);
+        //RaycastHit playerHit;
+
+        Ray playerRay = new Ray(transform.GetChild(0).position, -transform.up);
+        RaycastHit playerHit;
+
+        //レイを発射!!
+        if (Physics.Raycast(playerRay, out playerHit))
+        {
+            if (playerHit.transform.GetComponent<Walkable>() != null)
+            {
+                //足場を踏んでいる場合
+                currentCube = playerHit.transform;
+            }
+        }
     }
 }
--- Assets/Scripts/Walkable.cs
+++ Assets/Scripts/Walkable.cs
@@ -4,65 +4,55 @@ using UnityEngine;
 
 public class Walkable : MonoBehaviour
 {
-    //移動できるブロック座標
+
     public List<WalkPath> possiblePaths = new List<WalkPath>();
 
     [Space]
-    //道を探すときに通り過ぎた道
+
     public Transform previousBlock;
 
     [Space]
-    [Header("bool")]
-    [SerializeField,Tooltip("階段かどうか")]
-    private bool isStair = false;
-    //動くオブジェクトか
+
+    [Header("Booleans")]
+    public bool isStair = false;
     public bool movingGround = false;
-    [SerializeField, Tooltip("方向変更の有無")]
-    private bool dontRotate;
+    public bool isButton;
+    public bool dontRotate;
 
     [Space]
 
     [Header("Offsets")]
-    public float walkPointOffset =0.5f;
-    public float stairOffset = 0.4f;
+    public float walkPointOffset = .5f;
+    public float stairOffset = .4f;
 
-
-    /// <summary>
-    /// 歩く座標を求める
-    /// </summary>
     public Vector3 GetWalkPoint()
     {
-        //階段かどうか？
         float stair = isStair ? stairOffset : 0;
-        //座標を返す
         return transform.position + transform.up * walkPointOffset - transform.up * stair;
     }
 
-    /// <summary>
-    /// キューブの上にターゲットを描く
-    /// </summary>
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.gray;
-        float stair = isStair ? 0.4f : 0;
-        Gizmos.DrawSphere(GetWalkPoint(), 0.1f);
+        float stair = isStair ? .4f : 0;
+        Gizmos.DrawSphere(GetWalkPoint(), .1f);
 
-        //if (possiblePaths == null)
-        //    return;
+        if (possiblePaths == null)
+            return;
 
-        //foreach (WalkPath p in possiblePaths)
-        //{
-        //    if (p.target == null)
-        //        return;
-        //    Gizmos.color = p.active ? Color.black : Color.clear;
-        //    Gizmos.DrawLine(GetWalkPoint(), p.target.GetComponent<Walkable>().GetWalkPoint());
-        //}
+        foreach (WalkPath p in possiblePaths)
+        {
+            if (p.target == null)
+                return;
+            Gizmos.color = p.active ? Color.black : Color.clear;
+            Gizmos.DrawLine(GetWalkPoint(), p.target.GetComponent<Walkable>().GetWalkPoint());
+        }
     }
+}
 
-    [System.Serializable]
-    public class WalkPath
-    {
-        public Transform target;
-        public bool active = true;
-    }
+[System.Serializable]
+public class WalkPath
+{
+    public Transform target;
+    public bool active = true;
 }
