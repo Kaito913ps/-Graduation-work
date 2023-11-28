272acb6bafcb3c00ef7e347c7efd9d48234d7503
kaito913ps <kaito913jp@gmail.com>
2023-11-28 10:48:17 +0900

    AudioManagerを加えた

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static AudioManager instance;
+
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
+
+    //BGM用のクリップ配列
+    public AudioClip[] bgmClips;
+    //SE用のクリップ配列
+    public AudioClip[] sfxClips;
+
+    private Dictionary<string,AudioClip> bgmDict = new Dictionary<string, AudioClip> ();
+    private Dictionary<string,AudioClip> sfxDict = new Dictionary<string,AudioClip> ();
+
+    private void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            InitializeAudioClips();
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitializeAudioClips()
     {
-        
+        foreach(var clip in bgmClips)
+        {
+            bgmDict[clip.name] = clip;
+        }
+
+        foreach(var clip in sfxClips)
+        {
+            sfxDict[clip.name] = clip;
+        }
+    }
+
+    public void PlaySFX(string name)
+    {
+        if(sfxDict.TryGetValue(name, out var clip))
+        {
+            sfxSource.PlayOneShot(clip);
+        }
+    }
+
+    public void PlayBGM(string name)
+    {
+        if(bgmDict.TryGetValue(name,out var clip))
+        {
+            bgmSource.clip = clip;
+            bgmSource.Play();
+        }
     }
 }
